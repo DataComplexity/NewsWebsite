2ed376e0e8884da5bb0dedea3607cc1948faaffe
DataComplexity <lakshyasaini.work12345@gmail.com>
2026-05-08 18:37:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,20 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/home': 'http://localhost:5500',
+      '/topstories': 'http://localhost:5500',
+      '/explore': 'http://localhost:5500',
+      '/news': 'http://localhost:5500',
+      '/auth': 'http://localhost:5500',
+      '/stocks': 'http://localhost:5500',
+      '/crypto': 'http://localhost:5500',
+      '/weather': 'http://localhost:5500',
+      '/currency': 'http://localhost:5500',
+      '/guardian': 'http://localhost:5500',
+      '/searchResults': 'http://localhost:5500',
+      '/run': 'http://localhost:5500',
+    }
+  }
 })
